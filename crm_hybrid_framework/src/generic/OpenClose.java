73c709384d366382acc7ee4efd83d72764c0b1f8
igurupreeth <igurupreeth@gmail.com>
2024-10-08 16:10:06 +0530

    openclose class with annotation

--- crm_hybrid_framework/src/generic/OpenClose.java
+++ crm_hybrid_framework/src/generic/OpenClose.java
@@ -1,14 +1,16 @@
 package generic;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public abstract class OpenClose implements Constants
 {
 	public static WebDriver driver;
 	
+	@BeforeMethod
 	public static void openApplication()
 	{
 	    driver = new ChromeDriver();
@@ -18,6 +20,7 @@ public abstract class OpenClose implements Constants
 		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	}
 
+	@AfterMethod
 	public static void closeApplication()
 	{
 		driver.quit();
