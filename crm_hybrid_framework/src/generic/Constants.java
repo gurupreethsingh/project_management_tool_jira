72368a4314079f9a9b6182bd67092aa250787d47
igurupreeth <igurupreeth@gmail.com>
2024-10-08 14:36:22 +0530

    crm hybrid framework, constants interface added

---
+++ crm_hybrid_framework/src/generic/Constants.java
@@ -0,0 +1,8 @@
+package generic;
+
+public interface Constants 
+{
+	String urlOfApplication = "http://localhost:5173";
+	String rootFolder = System.getProperty("user.dir");
+	String excelSheetPath = rootFolder+"\\projectDocuments.xlsx";
+}
