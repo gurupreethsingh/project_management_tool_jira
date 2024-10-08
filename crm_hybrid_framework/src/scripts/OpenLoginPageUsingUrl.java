47fe957dbd436b6e6919d2d1c36f6dec518387b7
igurupreeth <igurupreeth@gmail.com>
2024-10-08 18:15:51 +0530

    login code third file

--- crm_hybrid_framework/src/scripts/OpenLoginPageUsingUrl.java
+++ crm_hybrid_framework/src/scripts/OpenLoginPageUsingUrl.java
@@ -1,7 +1,7 @@
 package scripts;
 
+import java.io.IOException;
 import org.testng.annotations.Test;
-
 import generic.Excel;
 import generic.OpenClose;
 import pom.LoginPage;
@@ -9,18 +9,19 @@ import pom.LoginPage;
 public class OpenLoginPageUsingUrl extends OpenClose
 {
 	@Test
-	public static void openHomePage() throws InterruptedException
+	public  void openHomePage() throws InterruptedException, IOException
 	{
-		LoginPage lp = new LoginPage();
+		LoginPage lp = new LoginPage(driver);
 		// expectedUrl http://localhost:5173/
 		String expectedUrl = (String) Excel.getData("crm_sheet1", 0, 0);
 		lp.verifyLoginPageUrl(expectedUrl);
 		
-		Thread.sleep(1000);
+		Thread.sleep(4000);
 		
 		String expectedLoginUrl = (String) Excel.getData("crm_sheet1", 1, 0);
 		driver.navigate().to(expectedLoginUrl);
 		lp.verifyLoginPageUrl(expectedLoginUrl);
+		Thread.sleep(4000);
 		
 	}
 }
