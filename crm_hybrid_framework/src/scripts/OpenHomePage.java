2949f6e4dc729b3c368b561c1de008909e66c0cc
igurupreeth <igurupreeth@gmail.com>
2024-10-08 18:15:25 +0530

    login code

--- crm_hybrid_framework/src/scripts/OpenHomePage.java
+++ crm_hybrid_framework/src/scripts/OpenHomePage.java
@@ -1,19 +1,16 @@
 package scripts;
 
+import java.io.IOException;
 import org.testng.annotations.Test;
-
 import generic.Excel;
 import generic.OpenClose;
 import pom.LoginPage;
 
-public class OpenHomePage extends OpenClose
-{
-   // when the user enters the url (localhost:5173) 
+public class OpenHomePage extends OpenClose{
 	
 	@Test
-	public static void openHomePage()
-	{
-		LoginPage lp = new LoginPage();
+	public void openHomePage() throws IOException	{
+		LoginPage lp = new LoginPage(driver);
 		// expectedUrl http://localhost:5173/
 		String expectedUrl = (String) Excel.getData("crm_sheet1", 0, 0);
 		lp.verifyLoginPageUrl(expectedUrl);
