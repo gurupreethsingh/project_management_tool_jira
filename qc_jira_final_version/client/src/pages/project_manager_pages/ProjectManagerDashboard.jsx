3b50c63d7548f08b4b1693f650a1c8555570d50f
igurupreeth <igurupreeth@gmail.com>
2024-10-16 11:48:05 +0530

    pattern 5 and pattern 6 start pattern programs.

---
+++ qc_jira_final_version/client/src/pages/project_manager_pages/ProjectManagerDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectManagerDashboard = () => {
+  return (
+    <div>
+      <h1>Propject Manager Dashboard</h1>
+    </div>
+  )
+}
+
+export default ProjectManagerDashboard
