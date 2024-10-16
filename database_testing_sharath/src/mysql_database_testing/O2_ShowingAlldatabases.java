cacb6978064ce2211e2139bc666c52427b38a2a0
sharusd <sharathdevang150@gmail.com>
2024-10-16 17:28:28 +0530

    updated

---
+++ database_testing_sharath/src/mysql_database_testing/O2_ShowingAlldatabases.java
@@ -0,0 +1,37 @@
+package mysql_database_testing;
+
+import java.sql.*;
+
+public class O2_ShowingAlldatabases {
+
+	public static void main(String[] args)
+	{
+		Connection conn = null;
+		
+		try
+		{
+			// try to connect mysql
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "root");
+			if(conn==null)
+			{
+				System.out.println("connection failed");
+			}
+			else
+			{
+				System.out.println("connected successfully to my sql database");
+				PreparedStatement ps = conn.prepareStatement("show databases");
+				ResultSet rs = ps.executeQuery();
+				while(rs.next())
+				{
+					  System.out.println(rs.getString(1));
+				}
+			}
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+
+	}
+
+}
