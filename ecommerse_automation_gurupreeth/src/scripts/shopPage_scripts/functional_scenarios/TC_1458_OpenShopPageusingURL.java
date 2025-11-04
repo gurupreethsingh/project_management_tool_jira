b7ddf8fbdcabc278ccd5aa10a5f0dcb78950f499
gurupreethsingh <igurupreeth@gmail.com>
2025-11-04 13:30:54 +0530

    updates

--- ecommerse_automation_gurupreeth/src/scripts/shopPage_scripts/functional_scenarios/TC_1458_OpenShopPageusingURL.java
+++ ecommerse_automation_gurupreeth/src/scripts/shopPage_scripts/functional_scenarios/TC_1458_OpenShopPageusingURL.java
@@ -1,13 +1,49 @@
 package scripts.shopPage_scripts.functional_scenarios;
 
+import java.io.File;
+import java.time.Duration;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import generic.AllVerifications;
+import generic.AutomationConstants;
 import generic.Excel;
-import generic.OpenClose_shopPage;
 
-public class TC_1458_OpenShopPageusingURL extends OpenClose_shopPage {
+
+
+ class OpenClose_Anusha implements AutomationConstants
+{
+	static String PATH_SHOP                = "/shop";
+	static String URL_SHOP                 = URL_OF_APPLICATION + PATH_SHOP;
+	String excelSheetPath = rootDirectory+File.separator+"project_documents_ANUSHA_shopPage.xlsx";
+	String failedScreenshots = rootDirectory + File.separator + "failedScreenshots";
+	public static WebDriver driver = null; 
+	
+	@BeforeMethod
+	public static void openApplication()
+	{
+	    driver = new ChromeDriver(); 
+	    driver.manage().window().maximize(); 
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	    driver.get(URL_SHOP);
+	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+	}
+	
+	
+	@AfterMethod
+	public static void closeApplication()
+	{
+		driver.quit();
+	}
+
+}
+public class TC_1458_OpenShopPageusingURL  extends OpenClose_Anusha{
 
 		SoftAssert sa = new SoftAssert();
 		
