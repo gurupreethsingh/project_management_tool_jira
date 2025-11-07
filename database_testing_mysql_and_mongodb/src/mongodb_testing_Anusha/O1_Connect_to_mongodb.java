22ae9465d4746fc0fb00b351943477b63212555d
ianushahn <poornimapushpa5@gmail.com>
2025-11-07 19:13:32 +0530

    updates

--- database_testing_mysql_and_mongodb/src/mongodb_testing_Anusha/O1_Connect_to_mongodb.java
+++ database_testing_mysql_and_mongodb/src/mongodb_testing_Anusha/O1_Connect_to_mongodb.java
@@ -2,13 +2,13 @@ package mongodb_testing_Anusha;
 
 import com.mongodb.client.MongoClients;
 
-public class O1_onnect_to_mongodb 
+public class O1_Connect_to_mongodb 
 {
 	public static void main (String[] args)
 	{
 		 com.mongodb.client.MongoClient connection = null;
 	        try {
-	            connection = MongoClients.create("mongodb://localhost:27017");        // connect
+	            connection = MongoClients.create("mongodb://localhost:27017");        // clients is the sub class of client
 
 	            if (connection != null) {
 	                System.out.println("Connected to MongoDB and it is reachable.");
--- database_testing_mysql_and_mongodb/src/mongodb_testing_Anusha/O3_Creating_new_database.java
+++ database_testing_mysql_and_mongodb/src/mongodb_testing_Anusha/O3_Creating_new_database.java
@@ -1,9 +1,12 @@
 package mongodb_testing_Anusha;
 
+import java.util.Scanner;
+
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoIterable;
 
-public class O2_List_all_databases 
+public class O3_Creating_new_database 
 {
 	public static void main (String[] args)
 	{
@@ -14,15 +17,39 @@ public class O2_List_all_databases
 	            if (connection != null) 
 	            {
 	                System.out.println("Connected to MongoDB and it is reachable.");  // if connected fetch all databases and print
+                   System.out.println("entere a new database name which u want to create");
+	                Scanner sc = new Scanner(System.in);
+	               String enteredDataBaseName=sc.nextLine();
+	                // taken input from user
+	                
+	                
 	                MongoIterable<String> allDatabaseNames = connection.listDatabaseNames();
-
+                    boolean dataBaseAlreadyExists=false;
 	                boolean databaseFetched = false; // ASSUMING there is no databases and count =0
 	                int count = 0; 
 	                
 	                
 	                for (String eachDataBasename : allDatabaseNames) // database is fetching one by one 
 	                {
-	                    System.out.println(" - " + eachDataBasename);
+	                 if(eachDataBasename.equals(enteredDataBaseName))
+	                 {
+	                	 dataBaseAlreadyExists = true;
+	                	 System.out.println("database "+ enteredDataBaseName +" already exists give other name ");
+	                	 break;
+	                 }
+	                 else
+	                 {
+	                	 // create DB by creating an empty collection
+                         MongoDatabase db = connection.getDatabase(enteredDataBaseName ); 
+                         db.createCollection("initial_collection");
+                         System.out.println("Database created successfully: " + enteredDataBaseName );
+                         System.out.println("An empty collection 'initial_collection' was created."); 
+                         // after creating  a new database lets print all database and check new data abse is created or not
+                         for( String eachDBnames:allDatabaseNames)
+                         {
+                        	 System.out.println(eachDBnames);
+                         }
+	                 }
 	                    count++;
  	                    databaseFetched = true;  //after fetcing database count will increase then databasefetched will be true 
 	                   
