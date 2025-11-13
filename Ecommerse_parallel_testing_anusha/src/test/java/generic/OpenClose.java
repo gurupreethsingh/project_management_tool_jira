c4c219dcf0d1781e03550a0016b6b3fb6c8ca5c3
ianushahn <poornimapushpa5@gmail.com>
2025-11-13 17:41:23 +0530

    parallel testing

--- Ecommerse_parallel_testing_anusha/src/test/java/generic/OpenClose.java
+++ Ecommerse_parallel_testing_anusha/src/test/java/generic/OpenClose.java
@@ -4,17 +4,21 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 public class OpenClose implements AutomationConstants
 {
-	public static WebDriver driver = null; 
+	public static WebDriver driver = DriverManager.getDriver(); 
 	
+	@Parameters("browser")
 	@BeforeMethod
 	public static void openApplication()
 	{
-	    driver = new ChromeDriver(); 
+	    driver = new ChromeDriver();
+	    DriverManager.setDriver(driver);
 	    driver.manage().window().maximize(); 
 	    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 	    driver.get(URL_HOME);
@@ -25,7 +29,11 @@ public class OpenClose implements AutomationConstants
 	@AfterMethod
 	public static void closeApplication()
 	{
-		driver.quit();
+		  driver =DriverManager.getDriver();
+		if(driver!=null) {
+			driver.quit();	
+		}
+		DriverManager.unload();
 	}
 
 }
